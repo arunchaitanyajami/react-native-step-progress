4f13a3016e03a54b3eb9b8e92cfed8a74e57f582
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-03-27 23:29:53 +0530

    Positiona changes

--- src/ProgressSteps/ProgressButtons.js
+++ src/ProgressSteps/ProgressButtons.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 
 const ProgressButtons = props => (
-  <View style={{ flexDirection: 'row', marginTop: 90 }}>
-    <View style={{ position: 'absolute', left: 0, bottom: 40 }}>{props.renderPreviousButton()}</View>
-    <View style={{ position: 'absolute', right: 0, bottom: 40 }}>{props.renderNextButton()}</View>
-  </View>
+    <View style={{ flexDirection: 'row', marginTop: 90 }}>
+        <View style={{ position: 'absolute', left: 0, bottom: 10 }}>{props.renderPreviousButton()}</View>
+        <View style={{ position: 'absolute', right: 0, bottom: 10 }}>{props.renderNextButton()}</View>
+    </View>
 );
 
 export default ProgressButtons;
