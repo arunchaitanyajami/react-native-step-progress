d43d916f3c7f9154de8a054712ba88bdb26c2a43
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-03-28 00:57:48 +0530

    Step change modification

--- src/ProgressSteps/ProgressButtons.js
+++ src/ProgressSteps/ProgressButtons.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View } from 'react-native';
 
 const ProgressButtons = props => (
-    <View style={{ flexDirection: 'row', marginTop: 90 , ...props.btnContainer }}>
-        <View style={{ position: 'absolute', left: 60, bottom: 40, ...props.previousBtnContainer }}>{props.renderPreviousButton()}</View>
-        <View style={{ position: 'absolute', right: 60, bottom: 40, ...props.nextBtnContainer }}>{props.renderNextButton()}</View>
+    <View style={{ flexDirection: 'row', ...props.btnContainer }}>
+        <View style={{ ...props.previousBtnContainer }}>{props.renderPreviousButton()}</View>
+        <View style={{ ...props.nextBtnContainer }}>{props.renderNextButton()}</View>
     </View>
 );
 
