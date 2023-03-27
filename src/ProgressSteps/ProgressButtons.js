66b8db85f09efbce3bf8341eb9e33452f7fe1013
Arun Chaitanya Jami <arunchaitanyajami@gmail.com>
2023-03-28 01:27:04 +0530

    Fix bottom btn issues

--- src/ProgressSteps/ProgressButtons.js
+++ src/ProgressSteps/ProgressButtons.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View } from 'react-native';
 
 const ProgressButtons = props => (
-    <View style={{ flexDirection: 'row', ...props.btnContainer }}>
-        <View style={{ ...props.previousBtnContainer }}>{props.renderPreviousButton()}</View>
-        <View style={{ ...props.nextBtnContainer }}>{props.renderNextButton()}</View>
+    <View style={{ flexDirection: 'row', marginTop: 40 , ...props.btnContainer }}>
+        <View style={{ position: 'absolute', left: 0, bottom: 20, ...props.previousBtnContainer }}>{props.renderPreviousButton()}</View>
+        <View style={{ position: 'absolute', right: 0, bottom: 20, ...props.nextBtnContainer }}>{props.renderNextButton()}</View>
     </View>
 );
 
